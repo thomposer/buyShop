573d9302099930b1e5624b82dc383c3a65b2bc7a
vynv@nal.vn <vynv@nal.vn>
2017-04-27 15:51:22 +0700

    commit header

--- src/components/Main/shop/Header.js
+++ src/components/Main/shop/Header.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
+import { View, Image, Text, TextInput, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
+import icMenu from '../../../media/appicon/ic_menu.png';
+import icLogo from '../../../media/appicon/ic_logo.png';
 
 const { height } = Dimensions.get('window');
 
 class Header extends Component {
-    state = {}
+
     render() {
+        const { wrapper, row1, textInput, iconStyle, titleStyle } = styles;
         return (
-            <View style={{ height: height / 8 }}>
-                <TouchableOpacity onPress={this.props.onOpen} >
-                    <Text>Open Menu</Text>
-                </TouchableOpacity>
+            <View style={wrapper} >
+                <View style={row1} >
+                    <TouchableOpacity onPress={this.props.onOpen}>
+                        <Image source={icMenu} style={iconStyle} />
+                    </TouchableOpacity>
+                    <Text style={titleStyle}>Wearing a Dress</Text>
+                    <Image source={icLogo} style={iconStyle} />
+                </View>
+                <TextInput 
+                style={textInput}
+                placeholder="What do you want to buy?" 
+                />
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    wrapper: { height: height / 8, backgroundColor: '#26b391', padding: 10, justifyContent: 'space-around' },
+    row1: { flexDirection: 'row', justifyContent: 'space-between' },
+    titleStyle: { color: '#FFF', fontSize: 20 },
+    textInput: { height: height / 25, backgroundColor: '#FFF', paddingLeft: 10 },
+    iconStyle: { height: 25, width: 25 }
+});
 export default Header;
